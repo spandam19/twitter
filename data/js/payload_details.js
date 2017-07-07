17f35338cdac76de492cdace58de484f746d7575
spandam19 <spandam19bamboo@gmail.com>
2017-07-07 17:20:33 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
-var payload_details =  {
-  "tweets" : 3441,
-  "created_at" : "2017-07-07 07:29:09 +0000",
-  "lang" : "ko"
+var payload_details = {
+	"tweets": 3443,
+	"created_at": "Fri Jul 07 08:20:32 +0000 2017",
+	"lang": "ko"
 }
