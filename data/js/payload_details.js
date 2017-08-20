d9492123ac2e4295157f51a3b22319ab2f5c3d52
spandam19 <spandam19bamboo@gmail.com>
2017-08-20 21:14:51 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5809,
-	"created_at": "Sun Aug 20 11:14:48 +0000 2017",
+	"tweets": 5810,
+	"created_at": "Sun Aug 20 12:14:49 +0000 2017",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_08.js",
 		"year": 2017,
 		"var_name": "tweets_2017_08",
-		"tweet_count": 654,
+		"tweet_count": 655,
 		"month": 8
 	},
 	{
