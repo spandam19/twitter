4f9ba499fcc6e36da3291a5a9b9b694cfabe3ded
spandam19 <spandam19bamboo@gmail.com>
2019-04-02 00:14:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8426,
-	"created_at": "Wed Mar 27 15:14:37 +0000 2019",
+	"tweets": 8427,
+	"created_at": "Mon Apr 01 15:14:38 +0000 2019",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2019_04.js
@@ -0,0 +1,177 @@
+Grailbird.data.tweets_2019_04 = 
+[
+	{
+		"created_at": "Mon Apr 01 15:13:56 +0000 2019",
+		"id": 1112612165599395800,
+		"id_str": "1112734848202866689",
+		"full_text": "혐스일-푸딩과 장관의 만우절 옷 바꿔 입기 / 앗씨 좋은데...? https://t.co/a3Qq1QXPOu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			36
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1112612079763021800,
+					"id_str": "1112612079763021824",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/D3DKyqsVYAA4HGX.png",
+					"media_url_https": "https://pbs.twimg.com/media/D3DKyqsVYAA4HGX.png",
+					"url": "https://t.co/a3Qq1QXPOu",
+					"display_url": "pic.twitter.com/a3Qq1QXPOu",
+					"expanded_url": "https://twitter.com/Haigidal/status/1112612165599395840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 665,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 691,
+							"h": 676,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 691,
+							"h": 676,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1112612079763021800,
+					"id_str": "1112612079763021824",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/D3DKyqsVYAA4HGX.png",
+					"media_url_https": "https://pbs.twimg.com/media/D3DKyqsVYAA4HGX.png",
+					"url": "https://t.co/a3Qq1QXPOu",
+					"display_url": "pic.twitter.com/a3Qq1QXPOu",
+					"expanded_url": "https://twitter.com/Haigidal/status/1112612165599395840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 665,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 691,
+							"h": 676,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 691,
+							"h": 676,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 254523017,
+			"id_str": "254523017",
+			"name": "Haigidal",
+			"screen_name": "Haigidal",
+			"location": "Korea",
+			"description": "다덕인의 트위터 / Do not repost my artwork on any other platform especially Pinterest . Ask me first if you want to use my artwork for something.",
+			"url": "https://t.co/ntZDdoDy9x",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ntZDdoDy9x",
+							"expanded_url": "http://teruvil.blog.me",
+							"display_url": "teruvil.blog.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1502,
+			"friends_count": 40,
+			"listed_count": 6,
+			"created_at": "Sat Feb 19 13:07:42 +0000 2011",
+			"favourites_count": 1067,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 287,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1108629012660707328/CjEaBRKN_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1108629012660707328/CjEaBRKN_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/254523017/1535352740",
+			"profile_link_color": "F37741",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 20,
+		"favorite_count": 37,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "혐스일-푸딩과 장관의 만우절 옷 바꿔 입기 / 앗씨 좋은데...? https://t.co/a3Qq1QXPOu"
+	}
+]
